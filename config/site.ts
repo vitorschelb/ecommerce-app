3cfc0ca8d219a84ad3e08dfe17e06c634fe30862
vitoraiala@gmail.com <vitoraiala@gmail.com>
2023-07-24 15:44:38 -0300

    "fea: metadata added to layout.tsx"

--- config/site.ts
+++ config/site.ts
@@ -1,8 +1,8 @@
 export type SiteConfig = typeof siteConfig
 
 export const siteConfig = {
-  name: "Name",
-  description: "Description",
+  name: "Flair",
+  description: "Expertly designed goods for the workspace, home and travel",
   footer: [
     { name: "Home", href: "/" },
     { name: "About", href: "#" },
