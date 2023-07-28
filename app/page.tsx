b8d2388da8d39cb0e661c97289d1c0c8be7bd712
vitoraiala@gmail.com <vitoraiala@gmail.com>
2023-07-28 12:14:04 -0300

    fea: implements search

--- app/page.tsx
+++ app/page.tsx
@@ -3,27 +3,52 @@ import { groq } from "next-sanity"
 
 import { SanityProduct } from "@/config/inventory"
 import { siteConfig } from "@/config/site"
-import { seedSanityData } from "@/lib/seed"
+// import { seedSanityData } from "@/lib/seed"
 import { cn } from "@/lib/utils"
 import { ProductFilters } from "@/components/product-filters"
 import { ProductGrid } from "@/components/product-grid"
 import { ProductSort } from "@/components/product-sort"
 
+// Declaração de interface para as props recebidas pelo componente.
 interface Props {
   searchParams: {
     date?: string
     price?: string
+    color?: string
+    category?: string
+    size?: string
+    search?: string
   }
 }
 
+// Componente principal que será exportado como padrão.
 export default async function Page({ searchParams }: Props) {
-  const { date = "desc", price } = searchParams
+  // Extração das variáveis searchParams (com valores padrão) para uso mais fácil.
+  const { date = "desc", price, color, category, size, search } = searchParams
+
+  // Construção de strings para ordenação da consulta no Sanity.
   const priceOrder = price ? `| order(price ${price})` : ""
   const dateOrder = date ? `| order(_createdAt ${date})` : ""
   const order = `${priceOrder}${dateOrder}`
-  // await seedSanityData()
+
+  // Construção das cláusulas de filtro para a consulta no Sanity.
+  const producFilter = `_type == "product"`
+  const colorFilter = color ? `&& "${color}" in colors` : ""
+  const categoryFilter = category ? `&& "${category}" in categories` : ""
+  const sizeFilter = size ? `&& "${size}" in sizes` : ""
+
+  // Construção da cláusula de filtro para busca por nome dos produtos.
+  const searchFilter = search ? `&& name match "${search}"` : ""
+
+  // Concatenação de todas as cláusulas para formar o filtro completo.
+  const filter = `*[${producFilter}${colorFilter}${categoryFilter}${sizeFilter}${searchFilter}]`
+
+  // Consulta ao Sanity para obter os produtos de acordo com os filtros e ordenação especificados.
+
+  // await seedSanityData() -> Para popular o Sanity com dados de teste.
+
   const products = await client.fetch<SanityProduct[]>(
-    groq`*[_type == "product"] ${order} {
+    groq`${filter} ${order} {
       _id,
       _createdAt,
       name,
@@ -35,9 +60,11 @@ export default async function Page({ searchParams }: Props) {
       "slug": slug.current
     }`
   )
-  // console.log(products)
+
+  // Renderização do componente na página.
   return (
     <div>
+      {/* Parte superior da página com o título e descrição do site */}
       <div className="px-4 pt-20 text-center">
         <h1 className="text-4xl font-extrabold tracking-normal">
           {siteConfig.name}
@@ -46,20 +73,25 @@ export default async function Page({ searchParams }: Props) {
           {siteConfig.description}
         </p>
       </div>
+
       <div>
+        {/* Componente principal */}
         <main className="mx-auto max-w-6xl px-6">
           <div className="flex items-center justify-between border-b border-gray-200 pb-4 pt-24 dark:border-gray-800">
             <h1 className="text-xl font-bold tracking-tight sm:text-2xl">
+              {/* Exibição do número de resultados encontrados */}
               {products.length} result{products.length > 1 && "s"}
             </h1>
-            {/* Product Sort */}
+            {/* Componente para ordenar os produtos */}
             <ProductSort />
           </div>
 
+          {/* Seção principal de exibição dos produtos */}
           <section aria-labelledby="products-heading" className="pb-24 pt-6">
             <h2 id="products-heading" className="sr-only">
               Products
             </h2>
+            {/* Grid para exibir os produtos */}
             <div
               className={cn(
                 "grid grid-cols-1 gap-x-8 gap-y-10",
@@ -69,10 +101,10 @@ export default async function Page({ searchParams }: Props) {
               )}
             >
               <div className="hidden lg:block">
-                {/* Product filters */}
+                {/* Componente de filtros de produtos (exibido apenas em tamanhos grandes) */}
                 <ProductFilters />
               </div>
-              {/* Product grid */}
+              {/* Componente de exibição dos produtos */}
               <ProductGrid products={products} />
             </div>
           </section>
--- components/site-header.tsx
+++ components/site-header.tsx
@@ -2,7 +2,7 @@
 
 import Link from "next/link"
 import { usePathname, useRouter, useSearchParams } from "next/navigation"
-import { Edit, ShoppingBag } from "lucide-react"
+import { Edit, ShoppingBag, ShoppingCart } from "lucide-react"
 import { useShoppingCart } from "use-shopping-cart"
 
 import { Button } from "@/components/ui/button"
@@ -11,14 +11,38 @@ import { MainNav } from "@/components/main-nav"
 import { ThemeToggle } from "@/components/theme-toggle"
 
 export function SiteHeader() {
+  // Uso de hooks para obter informações da URL e parâmetros de pesquisa (search params).
   const pathname = usePathname()
+  const router = useRouter()
+  const searchParams = useSearchParams()
+  const { cartCount } = useShoppingCart()
+  const defaultSearchQuery = searchParams.get("search") ?? "" // Obtenção do valor do parâmetro 'search' da URL.
+
+  // Se o caminho da URL começar com "/studio", o cabeçalho não será exibido (retornará null).
   if (pathname.startsWith("/studio")) return null
 
+  // Função chamada ao enviar o formulário de pesquisa.
+  function onSubmit(event: React.SyntheticEvent<HTMLFormElement>) {
+    event.preventDefault()
+    const formData = new FormData(event.currentTarget)
+    const searchQuery = formData.get("search") // Obtenção do valor digitado pelo usuário no campo de pesquisa.
+
+    // Atualização da URL para redirecionar para a página de pesquisa com o valor da pesquisa como parâmetro.
+    router.replace(`/?search=${searchQuery}`)
+  }
+
+  // Renderização do componente de cabeçalho (header) do site.
   return (
     <header className="sticky top-0 z-40 w-full border-b bg-background">
       <div className="mx-auto flex h-16 max-w-6xl items-center justify-between space-x-4 px-6 sm:space-x-0">
+        {/* Componente de navegação principal */}
         <MainNav />
-        <form className="hidden items-center lg:inline-flex">
+
+        {/* Formulário de pesquisa (exibido apenas em tamanhos grandes de tela) */}
+        <form
+          onSubmit={onSubmit}
+          className="hidden items-center lg:inline-flex"
+        >
           <Input
             id="search"
             name="search"
@@ -26,17 +50,25 @@ export function SiteHeader() {
             autoComplete="off"
             placeholder="Search products..."
             className="h-9 lg:w-[300px]"
+            defaultValue={defaultSearchQuery}
           />
         </form>
+
+        {/* Seção de ícones e botões no cabeçalho */}
         <div className="flex items-center space-x-1">
+          {/* Botão com ícone de carrinho de compras e contagem de itens no carrinho */}
           <Link href="/cart">
             <Button size="sm" variant="ghost">
               <ShoppingBag className="h-5 w-5" />
-              <span className="ml-2 text-sm font-bold">0</span>
+              <span className="ml-2 text-sm font-bold">{cartCount}</span>
               <span className="sr-only">Cart</span>
             </Button>
           </Link>
+
+          {/* Componente de alternância de tema */}
           <ThemeToggle />
+
+          {/* Botão com ícone de edição (exibido apenas em ambiente de desenvolvimento) */}
           {process.env.NODE_ENV === "development" && (
             <Link href="/studio">
               <Button size="sm" variant="ghost">
